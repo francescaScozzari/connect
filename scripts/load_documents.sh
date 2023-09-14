9e2f6e5f1263d7ab4e05e10ecd693cfe84120a77
Matteo Vitali <matteo@20tab.com>
2023-09-14 12:40:38 +0200

    Add suport for limit arg to load_documents

--- scripts/load_documents.sh
+++ scripts/load_documents.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Load documents to qdrant
+# Run ./scripts/load_documents.sh
+# If you want limit number of documents (for ex. 100):
+# Run ./scripts/load_documents.sh 100
+
 set -euo pipefail
 
+limit="${1-""}"
 date_timestamp=$(date +%Y%m%d%H%M)
 echo "STARTED AT: $(date '+%Y-%m-%d %H:%M:%S')" > ./data/load_documents_$date_timestamp.logs
 docker compose run --rm \
@@ -11,5 +17,5 @@ docker compose run --rm \
     --volume=$(pwd)/data/:/app/data/ \
     backend \
     /bin/bash -sc \
-    "python3 -m manage load_documents -v 3 >> /app/data/load_documents_$date_timestamp.logs"
+    "python3 -m manage load_documents -v 3 `if [ $limit != "" ]; then echo "--limit $limit"; fi` >> /app/data/load_documents_$date_timestamp.logs"
 echo "FINISHED AT: $(date '+%Y-%m-%d %H:%M:%S')" >> ./data/load_documents_$date_timestamp.logs
