284749984b6a0032cf2251cbe89d0687f9c3d80b
Paolo Melchiorre <paolo@20tab.com>
2023-09-21 15:35:40 +0000

    TG-525 Speed-up admins

--- scopus/admin.py
+++ scopus/admin.py
@@ -10,8 +10,11 @@ class ScopusAuthorAdmin(admin.ModelAdmin):
     """Scopus author's admin."""
 
     list_display = ("author_id", "full_name", "university", "orcid")
-    search_fields = ("author_id",)
     readonly_fields = ("author_id", "data", "full_name", "university", "orcid")
+    search_fields = ("author_id__exact",)
+    show_full_result_count = False
+    sortable_by: list = []
+    view_on_site = False
 
 
 @admin.register(ScopusDocument)
@@ -19,5 +22,8 @@ class ScopusDocumentAdmin(admin.ModelAdmin):
     """Scopus document's admin."""
 
     list_display = ("doi", "title")
-    search_fields = ("doi",)
     readonly_fields = ("doi", "data", "title", "description")
+    search_fields = ("doi__exact",)
+    show_full_result_count = False
+    sortable_by: list = []
+    view_on_site = False
--- xai/admin.py
+++ xai/admin.py
@@ -10,6 +10,20 @@ class TokensEmbeddingsAdmin(admin.ModelAdmin):
     """Explainable Artificial Intelligence tokens embeddings admin."""
 
     list_display = ("document",)
-    list_select_related = ("document",)
-    list_per_page = 25
     readonly_fields = ("document", "data")
+    search_fields = ("document__exact",)
+    show_full_result_count = False
+    sortable_by: list = []
+    view_on_site = False
+
+    def get_queryset(self, request):
+        """Return a QuerySet."""
+        queryset = (
+            super()
+            .get_queryset(request)
+            .select_related("document")
+            .defer("document__data")
+        )
+        if request.resolver_match.func.__name__ == "changelist_view":
+            queryset = queryset.defer("data")
+        return queryset
