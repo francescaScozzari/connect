4feb5c06303f57b6340d7b29bb9edd9832a740f4
Matteo Vitali <matteo@20tab.com>
2023-09-21 14:38:22 +0000

    TG-527 Increase gunicorn timeout to 120 sec

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -24,3 +24,8 @@ worker_class = "connect.workers.UvicornDjangoWorker"
 # https://docs.gunicorn.org/en/stable/settings.html#worker-tmp-dir
 
 worker_tmp_dir = "/dev/shm"  # nosec B108
+
+# Timeout
+# https://docs.gunicorn.org/en/stable/settings.html#timeout
+
+timeout = 120
